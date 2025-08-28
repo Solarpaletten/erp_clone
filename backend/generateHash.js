17cb93cff03ab20e02d258c8a18631317d5bcfa7
Solarpaletten <167139067+Solarpaletten@users.noreply.github.com>
2025-08-29 00:25:10 +0200

    delete clean fix file backend frontend

--- backend/generateHash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'pass123';
-const salt = 10;
-
-const hash = bcrypt.hashSync(password, salt);
-console.log('Password:', password);
-console.log('Hash:', hash);
